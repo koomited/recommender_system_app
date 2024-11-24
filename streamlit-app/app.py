08f671e877aca5b4db1b603bc95b41c31027448d
koomited <koomi@aims.ac.za>
2024-11-24 11:24:12 +0200

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -244,7 +244,7 @@ elif page=="Recommendations":
     
     links_data = pd.read_csv("links.csv")
 
-    movie_id = movies_df[movies_df["title"].str.contains("toy)]["movieId"]
+    movie_id = movies_df[movies_df["title"].str.contains("toy)]
     
     st.write(f""" {movie_id}
                  
