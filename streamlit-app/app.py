ef5c4170da993ea397d9de40fd9b4b083a91ea35
koomited <koomi@aims.ac.za>
2024-11-24 10:24:00 +0200

    Update app.py

--- streamlit-app/app.py
+++ streamlit-app/app.py
@@ -227,7 +227,7 @@ elif page=="Search":
             else:
                 st.write(f"""
                  <div class="title", style="text-align: center">
-                 <span style="font-size:32px;"> No content matched 👋</span>
+                 <span style="font-size:32px;"> No content matched "{keywords}" </span>
                  </div>
                  """, unsafe_allow_html=True)
 
